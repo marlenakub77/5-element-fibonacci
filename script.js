a2d1574f25be9803a9c9279d6c5d7cc6f0b2cfd0
marlenakub77 <96764884+marlenakub77@users.noreply.github.com>
2022-01-26 17:05:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function fibonacci(n) {
+
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
+
+console.log(fibonacci(2));
